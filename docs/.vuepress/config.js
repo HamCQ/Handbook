e638e13b4ffab66410fb6842226ce74c2cd9aa31
emin.lin <emin.lin@live.com>
2023-10-20 18:29:33 +0800

    feat:支持vuepress+vdoing

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,88 @@
+
+module.exports = {
+    lang: 'zh-CN',
+    logo: '/img/logo/favicon-wmioxuat.png',
+    title: 'HamCQ手册',
+    description: '业余无线电爱好者手册',
+    head: [
+        ['meta', { name: 'keywords', content: 'hamcq,hamcq手册,业余无线电，业余无线电手册'}]
+    ],
+    themeConfig: {
+      logo: '/img/logo/favicon-wmioxuat.png',
+      darkMode:true,
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '社区', link: 'https://forum.hamcq.cn', target:'_blank'},
+      ],
+      search: true,
+      searchMaxSuggestions: 10,
+      smoothScroll: true,
+      nextLinks: false,
+      prevLinks: false,
+      // sidebar: 'auto',
+      repo: 'https://github.com/HamCQ/Handbook',
+      repoLabel: '仓库',
+      docsBranch: 'main',
+      sidebar: 'structuring',
+      showToArticle:false,
+      updateBar:{
+        showToArticle: false, 
+        moreArticle: false
+      },
+      titleBadge:false,
+      pageStyle:"line",
+      social:
+      {
+        iconfontCssFile: String, 
+        icons: [
+          {
+            iconClass: "icon-youjian", 
+            title: "联系我们", 
+            link: "mailto:contact@hamcq.cn"
+          },
+          {
+            iconClass: "icon-github", 
+            title: "Github", 
+            link: "https://github.com/hamcq"
+          },
+          {
+            iconClass: "icon-zhihu", 
+            title: "知乎", 
+            link: "https://www.zhihu.com/column/c_1327659699995885568"
+          }
+        ]
+      },
+    },
+    plugins: [
+      '@vuepress/back-to-top',
+      ['@vuepress/medium-zoom',
+        {
+          selector: '.theme-vdoing-content img:not(.no-zoom)',
+          options: {
+            margin: 16
+          }
+        }
+      ],
+    ],
+    markdown: {
+      toc: {
+          includeLevel:[1, 2, 3, 4]
+      }
+    },
+    theme: 'vdoing',
+    
+    head: [
+      // 添加百度统计
+      [
+        "script",
+        {},
+        `var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?e3319e60407fb2a26747424971fd4c3f";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();`
+      ]
+    ]
+}
---
+++ docs/index.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroText: HamCQ手册
+tagline: 一起来编写可提供大家查阅的业余无线电手册吧~
+actionText: 开始浏览 →
+actionLink: /pages/dabbca/
+bannerBg: none
+postList: none
+hideRightBar: true
+
+features: # 可选的
+  - title: 🧬 知识管理
+    details: 拒绝碎片，沉浸阅读
+  - title: 🍃 简洁高效
+    details: 清爽页面，快速访问
+  - title: 🪁 社区运营
+    details: 文档开源，共同编辑
+---
